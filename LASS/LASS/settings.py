9a826e7fa7d2b00b88e1e788bf7f5822146165ba
hayder <hayder@modernecotech.com>
2019-05-20 22:11:05 +0100

    1

--- LASS/LASS/settings.py
+++ LASS/LASS/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'LASS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LASS',
+        'USER':'postgres',
+        'PASSWORD':'1',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
