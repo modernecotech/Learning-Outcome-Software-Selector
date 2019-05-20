4ca162e7f0a4e1c5a05a8214c60a675175b3c08d
hayder <hayder@modernecotech.com>
2019-05-20 23:19:55 +0100

    3

--- LASS/LearningOutcomeSelector/models.py
+++ LASS/LearningOutcomeSelector/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Learning Outcomes Software Selector model is structured on a semantic basis
+
+class LearningOutcomes(models.Model):
+    LearningOutcomeDescription_text= models.CharField(max_length=100)
+    LearningCategories_field=models.ManyToManyField(
+        LearningoutcomesList,
+        through=''
+    )
+
+class LearningOutcomeList(models.Model):
+    name_text = models.CharField(max_length=200)
+
+class Application(models.Model):
+    ApplicationName_text=models.CharField(max_length=200)
+    Application_url=models.URLField()
+    Application_logo_image=models.ImageField()
+
+class ApplicationFeatures(models.Model):
+    FeatureName_text = models.CharField(max_length=200)
+    FeatureCategory=
+
