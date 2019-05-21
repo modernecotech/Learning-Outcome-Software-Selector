30e3b38343e9774ff1c7a319155a1b2ec5047d2e
hayder <hayder@modernecotech.com>
2019-05-21 14:05:23 +0100

    added REST Api

--- LASS/LearningOutcomeSelector/views.py
+++ LASS/LearningOutcomeSelector/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+#stuff for the REST framework
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
 def index(request):
