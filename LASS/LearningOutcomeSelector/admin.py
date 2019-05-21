d59feb7f739500fb43363b3b0bac404fd6ce1218
hayder <hayder@modernecotech.com>
2019-05-21 12:39:45 +0100

    dataEntryInterfaceADmin

--- LASS/LearningOutcomeSelector/admin.py
+++ LASS/LearningOutcomeSelector/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import LearningOutcomes, LearningOutcomeSubject, Knowledge, Competences, Skills, ApplicationFeatures, Application, ExistingApplicationUsers, ApplicationReviews
 
 # Register your models here.
+
+#LearningOutcomes, LearningOutcomeSubject, 
+#Knowledge, Competences, Skills, ApplicationFeatures, Application, ExistingApplicationUsers,
+#ApplicationReviews
+
+admin.site.register(LearningOutcomes)
+admin.site.register(LearningOutcomeSubject)
+admin.site.register(Knowledge)
+admin.site.register(Competences)
+admin.site.register(Skills)
+admin.site.register(ApplicationFeatures)
+admin.site.register(Application)
+admin.site.register(ExistingApplicationUsers)
+admin.site.register(ApplicationReviews)
