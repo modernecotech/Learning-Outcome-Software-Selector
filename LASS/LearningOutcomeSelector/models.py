6bf281ab5b71429aec797414a387a086cad8a82c
hayder <hayder@modernecotech.com>
2019-05-21 13:06:33 +0100

    update Model output

--- LASS/LearningOutcomeSelector/models.py
+++ LASS/LearningOutcomeSelector/models.py
@@ -7,11 +7,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class LearningOutcomes(models.Model):
     LearningOutcomeDescription_text= models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.LearningOutcomeDescription_text
+
 class LearningOutcomeSubject(models.Model):
     LearningOutcomeSubjectName_text = models.CharField(max_length=200)
     LinkedOutcome = models.ManyToManyField(
         LearningOutcomes
     )
+
+    def __str__(self):
+        return self.LearningOutcomeSubjectName_text
 
 class Knowledge(models.Model):
     KnowledgeName_text=models.CharField(max_length=200)
@@ -19,11 +25,16 @@ class Knowledge(models.Model):
         LearningOutcomeSubject
     )
 
+    def __str__(self):
+        return self.KnowledgeName_text
+
 class Competences(models.Model):
     CompetenceName_text = models.CharField(max_length=200)
     LinkedKnowledge=models.ManyToManyField(
         Knowledge
     )
+    def __str__(self):
+        return self.CompetenceName_text
 
 class Skills(models.Model):
     skillName_text=models.CharField(max_length=200)
@@ -31,11 +42,17 @@ class Skills(models.Model):
         Competences
     )
 
+    def __str__(self):
+        return self.skillName_text
+
 class ApplicationFeatures(models.Model):
     FeatureName_text = models.CharField(max_length=200)
     LinkedSkills=models.ManyToManyField(
         Skills
     )
+
+    def __str__(self):
+        return self.FeatureName_text
 
 class Application(models.Model):
     ApplicationName_text=models.CharField(max_length=200)
@@ -46,6 +63,9 @@ class Application(models.Model):
         ApplicationFeatures
     )
 
+    def __str__(self):
+        return self.ApplicationName_text
+
 class ExistingApplicationUsers(models.Model):
     ApplicationUser=models.CharField(max_length=200)
     ApplicationUser_url=models.URLField()
@@ -53,10 +73,16 @@ class ExistingApplicationUsers(models.Model):
         Application
     )
 
+    def __str__(self):
+        return self.ApplicationUser
+
 class ApplicationReviews(models.Model):
     ReviewPerson=models.CharField(max_length=200)
     ReviewText=models.TextField(max_length=1000)
     ReviewDate=models.DateTimeField(auto_now=True)
     ReviewRating=models.IntegerField(validators=[MaxValueValidator(10),MinValueValidator(0)])
     ReviewedApplication=models.ForeignKey(Application,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ReviewPerson
 
