5bfbca1aa9d356f04eae37a5016ea62bd3d5d29e
hayder <hayder@modernecotech.com>
2019-05-21 11:54:27 +0100

    updated model migrated

--- LASS/LASS/settings.py
+++ LASS/LASS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'LearningOutcomeSelector.apps.LearningoutcomeselectorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
